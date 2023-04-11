1f2cbd6f6706ef8a1e757016b7a25d4396264d24
pluiedev <hi@pluie.me>
2023-04-11 13:59:04 +0800

    refactors

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use deadqueue::limited::Queue;
 use eyre::{Context, Result};
-use fantoccini::{elements::Element, Client, ClientBuilder, Locator};
+use fantoccini::{elements::Element, Client, ClientBuilder, Locator, wd::Capabilities};
 use futures_util::{StreamExt, TryStreamExt};
-use std::{collections::HashMap, process::Stdio, sync::Arc};
+use std::{collections::HashMap, process::Stdio, sync::Arc, time::Duration};
 use tokio::{
     fs::File,
     io::{AsyncBufReadExt, BufReader},
     process::Command,
     sync::watch,
-    task::JoinSet,
+    task::JoinSet, time::timeout,
 };
 use tracing::*;
 
-#[derive(Clone, Default)]
+#[derive(Clone, Debug, Default)]
 struct ElementProcessor {
     freq: HashMap<String, usize>,
     window_width: u64,
@@ -29,7 +29,7 @@ impl ElementProcessor {
     }
     async fn process_node(mut self, v: Element) -> Result<Self> {
         let Ok(name) = v.tag_name().await else {
-            warn!(?v, "Unable to get name for element - perhaps it has already been removed from the DOM?");
+            warn!(v = ?v.element_id(), "Unable to get name for element - perhaps it has already been removed from the DOM?");
             return Ok(self);
         };
 
@@ -61,37 +61,37 @@ async fn main() -> Result<()> {
     let (tx, rx) = watch::channel(());
     ctrlc::set_handler(move || tx.send(()).unwrap())?;
 
-    let queue = Arc::new(Queue::<String>::new(10));
+    let mut caps = serde_json::map::Map::new();
+    let args = serde_json::json!({
+        "args": ["--headless", "--disable-gpu"]
+    });
+    caps.insert("moz:firefoxOptions".to_owned(), args.clone());
+    caps.insert("goog:chromeOptions".to_owned(), args.clone());
 
+    let queue = Arc::new(Queue::<String>::new(10));
     let mut set = JoinSet::new();
 
     for port in 4444..=4446 {
         let queue = Arc::clone(&queue);
         let rx = rx.clone();
+        let caps = caps.clone();
+
         set.spawn(async move {
-            let instance = Instance::new(port, queue).await.unwrap();
+            let instance = Instance::new(port, queue, caps).await.unwrap();
             instance.run(rx).await
         });
     }
 
-    let mut sites = BufReader::new(File::open("top-1m.csv").await?);
-    let mut buf = String::new();
+    tokio::spawn(queuer(queue.clone(), rx.clone()));
 
     loop {
         tokio::select! {
-            Some(Ok(res)) = set.join_next() => {
-                let res: ElementProcessor = res?;
+            Some(res) = set.join_next() => {
+                let res: ElementProcessor = res??;
                 info!(?res.freq);
             }
-            line = sites.read_line(&mut buf), if queue.len() < 10 => {
-                _ = line?;
-                let Some((_, site)) = buf.split_once(",") else { continue; };
-                let site = format!("https://{}", site.trim());
-
-                queue.push(site).await;
-                buf.clear();
-            }
             else => {
+                info!("Everything done! Exiting...");
                 break;
             }
         }
@@ -99,6 +99,33 @@ async fn main() -> Result<()> {
 
     Ok(())
 }
+
+type ShutdownRx = watch::Receiver<()>;
+
+#[tracing::instrument]
+async fn queuer(queue: Arc<Queue<String>>, mut rx: ShutdownRx) -> Result<()> {
+    let mut sites = BufReader::new(File::open("top-10.csv").await?);
+    let mut buf = String::new();
+
+    loop {
+        tokio::select! {
+            _ = rx.changed() => break,
+            line = sites.read_line(&mut buf) => {
+                if line? == 0 {
+                    break;
+                }
+                let Some((_, site)) = buf.split_once(",") else { continue; };
+                let site = format!("https://{}", site.trim());
+
+                queue.push(site).await;
+                buf.clear();
+            }
+        }
+    }
+
+    Ok(())
+}
+
 
 struct Instance {
     port: u32,
@@ -108,8 +135,8 @@ struct Instance {
     queue: Arc<Queue<String>>,
 }
 impl Instance {
-    #[tracing::instrument(skip(queue))]
-    async fn new(port: u32, queue: Arc<Queue<String>>) -> Result<Self> {
+    #[tracing::instrument(skip_all, fields(port = port))]
+    async fn new(port: u32, queue: Arc<Queue<String>>, capabilities: Capabilities) -> Result<Self> {
         info!("Initializing crawler instance");
 
         let log_path = format!("webdriver-{port}.log");
@@ -126,6 +153,7 @@ impl Instance {
 
         let url = format!("http://localhost:{port}");
         let client = ClientBuilder::native()
+            .capabilities(capabilities)
             .connect(&url)
             .await
             .wrap_err("failed to connect to WebDriver!")?;
@@ -145,14 +173,17 @@ impl Instance {
     }
 
     #[tracing::instrument(skip_all, fields(port = self.port))]
-    async fn run(mut self, mut shutdown_rx: watch::Receiver<()>) -> Result<ElementProcessor> {
+    async fn run(mut self, mut shutdown_rx: ShutdownRx) -> Result<ElementProcessor> {
         loop {
             tokio::select! {
                 _ = shutdown_rx.changed() => {
-                    info!("Shutdown received (work either exhausted or aborted) - exiting");
+                    info!("Shutdown received - exiting");
                     break;
                 }
-                Err(e) = self.crawl_loop() => error!("Error while crawling site: {e}")
+                res = self.crawl_loop() => match res {
+                    Ok(()) => break,
+                    Err(e) => error!("Error while crawling site: {e}")
+                }
             }
         }
         self.client.close().await?;
@@ -165,9 +196,15 @@ impl Instance {
         loop {
             info!(?self.port, "Crawler waiting for work");
 
-            let site = self.queue.pop().await;
-            self.crawl(site).await?;
+            match timeout(Duration::from_secs(3), self.queue.pop()).await {
+                Ok(site) => self.crawl(site).await?,
+                Err(_) => {
+                    info!("No work arrived in a while, exiting");
+                    break;
+                }
+            }
         }
+        Ok(())
     }
 
     #[tracing::instrument(skip(self))]
